85cd64606008257cc955c681598965fb7a2e6c24
idharrycloud <71224122+idharrycloud@users.noreply.github.com>
2020-09-13 23:13:07 +0700

    This is an update for IDN EKS Workshop

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Indonesia. This is EKS Workshop"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
